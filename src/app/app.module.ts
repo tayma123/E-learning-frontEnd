560ff21f91584775994ef8b331a4e606a403c900
tayma ben hmida <taymabenhmida1@gmail.com>
2021-08-03 11:55:31 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+
+import {CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,FormsModule, RouterModule,CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/user.ts
@@ -0,0 +1,12 @@
+export class User{
+    id: number;
+    username: string;
+    password: string;
+    dateNaissance:string;
+    nom:string;
+    prenom:string;
+    role: string;
+    actived:Boolean ;
+}
+
+
